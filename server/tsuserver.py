ed74aa1b54530c3e8f83697a7a0b03c3eb5683c1
Chrezm <TheChrezm@gmail.com>
2021-10-08 20:06:52 -0400

    Start of 4.3.1 development

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -67,9 +67,9 @@ class TsuserverDR:
 
         self.release = 4
         self.major_version = 3
-        self.minor_version = 0
-        self.segment_version = 'post1'
-        self.internal_version = '211006a'
+        self.minor_version = 1
+        self.segment_version = 'a1'
+        self.internal_version = 'm211008a'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
